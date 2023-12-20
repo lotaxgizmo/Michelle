11adb2523ee4525d19a43d0d87452dab3cfd9c21
Lotachi <lotaoko2@gmail.com>
2023-12-20 08:36:15 +0100

    k

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Mobile from './Mobile'
 
 function Header() {
     return (
-        <div className='mt-5`'>
+        <div className='my-5`'>
             <Desktop />
             <div className='Header flex justify-center align-center w-full self-center fixed top-1 z-20'>
                 <Mobile />
--- src/components/Proof.js
+++ src/components/Proof.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import proof from '../assets/proof.png'
-
 import obamic from '../assets/obamic.png'
+import pic1 from '../assets/pic1.png'
+import pic2 from '../assets/pic2.png'
+import pic3 from '../assets/pic3.png'
+import pic4 from '../assets/pic4.png'
+import pic5 from '../assets/pic5.png'
+import pic6 from '../assets/pic6.png'
+
 
 function Proof() {
     return (
@@ -9,11 +15,14 @@ function Proof() {
             <img src={proof} alt="" className="w-fit" />
             <div className="gallery border-2 border-[#EBF752] rounded-2xl lg:rounded-[60px] py-20 w-full flex flex-col
             items-center align-middle justify-center">
-                <img src={obamic} alt="" className="w-52 lg:w-fit py-2" />
-                <img src={obamic} alt="" className="w-52 lg:w-fit py-2" />
-                <img src={obamic} alt="" className="w-52 lg:w-fit py-2" />
-                <img src={obamic} alt="" className="w-52 lg:w-fit py-2" />
-
+                <div className="flex">
+                    <img src={pic1} alt="" className="w-52 lg:w-fit p-2" />
+                    <img src={pic2} alt="" className="w-52 lg:w-fit p-2" />
+                </div>
+                <img src={pic3} alt="" className="w-52 lg:w-fit p-2" />
+                <img src={pic4} alt="" className="w-52 lg:w-fit p-2" />
+                <img src={pic5} alt="" className="w-52 lg:w-fit p-2" />
+                <img src={pic6} alt="" className="w-52 lg:w-fit p-2" />
 
             </div>
         </div>
